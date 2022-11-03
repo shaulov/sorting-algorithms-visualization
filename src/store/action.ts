cf07e7eca5efd5f614d96faa4a89485c4f55047e
shaulov <shineginn@gmail.com>
2022-11-03 12:44:03 +0300

    Add actions for comparison

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,5 +1,17 @@
 import { createAction } from '@reduxjs/toolkit';
 
-export const setArray = createAction<number[], 'ARRAY/SET_ARRAY'>('ARRAY/SET_ARRAY');
+export const setArray = createAction<number[]>('ARRAY/SET_ARRAY');
 
-export const setLength = createAction<number, 'ARRAY/SET_LENGTH'>('ARRAY/SET_LENGTH');
+export const setLength = createAction<number>('ARRAY/SET_LENGTH');
+
+export const reset = createAction('COMPARISON/RESET');
+
+export const toggleSort = createAction<boolean>('COMPARISON/TOGGLE_SORT');
+
+export const setSortingSpeed = createAction<number>('COMPARISON/SET_SORTING_SPEED');
+
+export const setActiveElements = createAction<number[]>('COMPARISON/SET_ACTIVE_ELEMENTS');
+
+export const setAdditionalElements = createAction<number[]>('COMPARISON/SET_ADDITIONAL_ELEMENTS');
+
+export const setSortedElements = createAction<number[]>('COMPARISON/SET_SORTED_ELEMENTS');
