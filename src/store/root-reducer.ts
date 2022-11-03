6efb48f45f18d7ff240a9b745f23cbdcfc25ee8b
shaulov <shineginn@gmail.com>
2022-11-03 12:46:58 +0300

    Add comparison slice to root reducer

--- src/store/root-reducer.ts
+++ src/store/root-reducer.ts
@@ -1,7 +1,9 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import { arraySlice } from './array-process/array-process';
+import { comparisonSlice } from './comparison-process/comparison-process';
 import { NameSpace } from '../const';
 
 export const rootReducer = combineReducers({
   [NameSpace.Array]: arraySlice.reducer,
+  [NameSpace.Comparison]: comparisonSlice.reducer,
 });
