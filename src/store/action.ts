745d12cc71a851f1a105c11b42cb7b95d7a14747
shaulov <shineginn@gmail.com>
2022-11-17 02:36:32 +0300

    Add action for setNewParams

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,3 +1,9 @@
 import { createAction } from '@reduxjs/toolkit';
 
+interface IParams {
+  [identifire: number]: number;
+}
+
+export const setNewParams = createAction<IParams>('SORTING/SET_NEW_PARAMS');
+
 export const bubbleSort = createAction<number[]>('SORTING/BUBBLE');
